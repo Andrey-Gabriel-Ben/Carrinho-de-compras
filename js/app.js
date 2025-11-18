1b3be4a2a5cb96853c83b79e0951eae655136320
andrey <andreygabrielben@gmail.com>
2025-11-17 22:33:28 -0300

    Adicionar variáveis que armazenem os dados dos inputs

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+function adicionar() {
+    var quantidade = document.getElementById("quantidade");
+    var produto = document.getElementById("produto");
+    var valor = produto.value.match(/\d+/g);
+
+}
