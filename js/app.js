27076a37d824e26b1ff15263cafaf5f2a6fbab5b
andrey <andreygabrielben@gmail.com>
2025-11-18 23:02:52 -0300

    Adicionar uma lista que armazena o valor de cada montante adicionado ao carrinho

--- js/app.js
+++ js/app.js
@@ -2,12 +2,22 @@ var quantidadeGetID = document.getElementById("quantidade");
 var produtoGetID = document.getElementById("produto");
 var carrinhoGetID = document.getElementById("lista-produtos")
 
+var valorDoCarrinho = [];
 
 function adicionar() {
     let quantidade = quantidadeGetID.value
-    let valor = produtoGetID.value.match(/\d+/g);
+    let valor = parseInt(produtoGetID.value.match(/\d+/g));
     let produto = produtoGetID.value.replace(` - R$${valor}`, "")
+    let subtotal = valor*quantidade;
 
+    valorDoCarrinho.push(subtotal);
 
+    /*
+    console.log(produto)
+    console.log(quantidade)
+    console.log(valor)
+    console.log(subtotal)
+    console.log(valorDoCarrinho)
+    */
 
 }
