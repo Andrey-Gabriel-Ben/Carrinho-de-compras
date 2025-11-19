156a776661417940616ffd26d102166d65f4942f
andrey <andreygabrielben@gmail.com>
2025-11-18 22:23:25 -0300

    renomear as variáveis globais e adicionar novas internamente na função "adicionar()" tratando melhor os valores puxados do arquivo .HTML

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
+var quantidadeGetID = document.getElementById("quantidade");
+var produtoGetID = document.getElementById("produto");
+var carrinhoGetID = document.getElementById("lista-produtos")
+
 
 function adicionar() {
-    var quantidade = document.getElementById("quantidade");
-    var produto = document.getElementById("produto");
-    var valor = produto.value.match(/\d+/g);
+    let quantidade = quantidadeGetID.value
+    let valor = produtoGetID.value.match(/\d+/g);
+    let produto = produtoGetID.value.replace(` - R$${valor}`, "")
+
+
 
 }
